1f1de447e29a62a3e95512d68e26fd65d2992bc2
pixy-x <pixyxgit@gmail.com>
2023-01-22 16:12:19 +0300

    todo list

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,11 @@ export default function Home() {
   const [todo, setTodo] = useState("");
   const [desc, setDesc] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(todo);
+  };
+
   return (
     <Container className="pt-8">
       <h1>What do you want to do today?</h1>
@@ -20,7 +25,7 @@ export default function Home() {
         taking notes is your life&apos;s number one planner{" "}
         <Emoji symbol={"😉"} />
       </p>
-      <form className="my-4 block">
+      <form className="my-4 block" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Type here.."
@@ -41,6 +46,37 @@ export default function Home() {
           Create New To-do
         </button>
       </form>
+      <span className="sub-heading">todo list</span>
+      <div className="flex flex-col gap-4">
+        <div className="relative bg-slate-100 p-4 border rounded-lg">
+          <span className="absolute top-5 left-4 w-6 h-6 text-xs bg-slate-800 text-white rounded-full inline-flex items-center justify-center font-bold pt-[.85px] shadow-2xl">
+            1{" "}
+          </span>
+          <div className="pl-10">
+            <h4 className="text-slate-800 text-2xl">Wash the Dishes</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+              aperiam commodi cum cupiditate dolorem eum expedita id, illum
+              impedit, inventore maiores necessitatibus nemo nesciunt nobis odio
+              officiis rem similique ullam? lorem
+            </p>
+          </div>
+        </div>
+        <div className="relative bg-slate-100 p-4 border rounded-lg">
+          <span className="absolute top-5 left-4 w-6 h-6 text-xs bg-slate-800 text-white rounded-full inline-flex items-center justify-center font-bold pt-[.85px] shadow-2xl">
+            2{" "}
+          </span>
+          <div className="pl-10">
+            <h4 className="text-slate-800 text-2xl">Wash the Dishes</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+              aperiam commodi cum cupiditate dolorem eum expedita id, illum
+              impedit, inventore maiores necessitatibus nemo nesciunt nobis odio
+              officiis rem similique ullam? lorem
+            </p>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
