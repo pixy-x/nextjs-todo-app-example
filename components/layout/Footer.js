93507fea76297840833b0a8404c593d80d5e463e
pixy-x <pixyxgit@gmail.com>
2023-01-22 16:24:09 +0300

    footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,5 +1,23 @@
+import Container from "@/components/Container";
+import Emoji from "@/components/Emoji";
+
 function Footer() {
-  return <footer></footer>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="my-4">
+      <Container className="py-4">
+        <p className="leading-6 tracking-tight text-slate-900 opacity-60">
+          &copy; {currentYear} pixyxgit/next - All rights reserved. Please
+          contact us if you want to improve or help this app's code system.{" "}
+          <a href="https://github.com/pixy-x/nextjs-todo-app-example">
+            Click here
+          </a>{" "}
+          to see git repo <Emoji symbol={"👍"} />
+        </p>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
