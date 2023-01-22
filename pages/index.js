cf28b24a0833b525bae2479173373b96664d89a6
pixy-x <pixyxgit@gmail.com>
2023-01-22 15:46:22 +0300

    vercel

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [todos, setTodos] = useState([]);
+
   const [todo, setTodo] = useState("");
   const [desc, setDesc] = useState("");
 
@@ -11,12 +12,13 @@ export default function Home() {
     <Container className="pt-8">
       <h1>What do you want to do today?</h1>
       <p>
-        Today or tomorrow—it doesn't matter, whatever you want to do or plan to
-        do, create, organize and share them here.{" "}
+        Today or tomorrow—it doesn&apos;t matter, whatever you want to do or
+        plan to do, create, organize and share them here.{" "}
         <u>
           <i>Remember:</i>
         </u>{" "}
-        taking notes is your life's number one planner <Emoji symbol={"😉"} />
+        taking notes is your life&apos;s number one planner{" "}
+        <Emoji symbol={"😉"} />
       </p>
       <form className="my-4 block">
         <input
